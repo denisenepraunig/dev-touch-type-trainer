70c65254a3421e0fe1f9f439505e4185321035a0
Denise Nepraunig <denise.nepraunig@gmail.com>
2021-04-05 13:03:53 +0200

    Initial Commit

---
+++ dev-type-trainer/dev_type_trainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  dev_type_trainerApp.swift
+//  dev-type-trainer
+//
+//  Created by Denise Nepraunig on 05.04.21.
+//
+
+import SwiftUI
+
+@main
+struct dev_type_trainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
